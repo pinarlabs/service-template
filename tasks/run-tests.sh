db3364fe2a8d2ad3e119aa150a0c065030697348
pandocreek <bruj1t4oss@gmail.com>
2020-10-15 17:09:09 -0300

    update bash script

--- tasks/run-tests.sh
+++ tasks/run-tests.sh
@@ -1,10 +1,12 @@
 if [ -v $GITHUB_REPOSITORY ]
 then
+    echo "RUNNING LOCAL"
     ./tasks/db-tear-up.sh
     NODE_ENV=test jest --detectOpenHandles --forceExit --testPathPattern=tests
     errCode=$?
     ./tasks/db-tear-down.sh
 else
+    echo "RUNNING ON GITHUB ACTIONS"
     NODE_ENV=test jest --detectOpenHandles --forceExit --testPathPattern=tests
     errCode=$?
 fi
