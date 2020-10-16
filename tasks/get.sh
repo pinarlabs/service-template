9f1641a40f123109ad2ceb6a0b1a75e2c94f5ec1
pandocreek <bruj1t4oss@gmail.com>
2020-10-16 00:57:07 -0300

    update installation script

--- tasks/get.sh
+++ tasks/get.sh
@@ -1,5 +1,7 @@
 curl https://codeload.github.com/pinarlabs/service-template/zip/main --output dl.zip
 unzip dl.zip
+rm service-template/README.md
+rm service-template/tasks/get.sh
 cp -a service-template/. .
 rm -r service-template
 rm dl.zip
