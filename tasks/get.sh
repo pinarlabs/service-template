24c241bcc449839f9ea80aaf74d76c033565bf73
pandocreek <bruj1t4oss@gmail.com>
2020-10-16 01:04:29 -0300

    add main branch to installation script

--- tasks/get.sh
+++ tasks/get.sh
@@ -1,7 +1,7 @@
 curl https://codeload.github.com/pinarlabs/service-template/zip/main --output dl.zip
 unzip dl.zip
-rm service-template/README.md
-rm service-template/tasks/get.sh
-cp -a service-template/. .
-rm -r service-template
+rm service-template-main/README.md
+rm service-template-main/tasks/get.sh
+cp -a service-template-main/. .
+rm -r service-template-main
 rm dl.zip
